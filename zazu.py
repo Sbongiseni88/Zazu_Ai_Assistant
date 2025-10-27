81d8d736ab7ed2160e26f7aa80835aa49a15575c
Sibongiseni <sidubazjhb024@student.wethinkcode.co.za>
2025-10-27 23:01:11 +0200

    completed twilio reciver, recieves data from twilio and adds it to audio queue to be processed

--- zazu.py
+++ zazu.py
@@ -36,7 +36,35 @@ async def sts_receiver(sts_ws,twilio_ws,streamsid_queue):
 
 async def twilio_receiver(twilio_ws,audio_queue,streamsid_queue):
     BUFFER_SIZE=20*160
-    
+    inbuffer=bytearray(b"")
+
+    async for message in twilio_ws:
+        try:
+            data=json.loads(message)
+            event= data["event"]
+
+            if event=="start":
+                print('get our streamsid')
+                start=data["start"]
+                streamsid=start["streamsid"]
+                streamsid_queue.put_nowait(streamsid)
+            elif event=="connected":
+                continue
+            elif event=="media":
+                media=data["media"]
+                chunk=base64.b64decode(media["payload"])
+                if media["track"]=="inbound":
+                    inbuffer.extend(chunk)
+            elif event=="stop":
+                break
+
+            while len(inbuffer)>=BUFFER_SIZE:
+                chunk=inbuffer[:BUFFER_SIZE]
+                audio_queue.put_nowait(chunk)
+                inbuffer=inbuffer[BUFFER_SIZE:]
+        except:
+            break
+
 
 async def twilio_handler(twilio_ws):
     audio_queue=asyncio.Queue()
