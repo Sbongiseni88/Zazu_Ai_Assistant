b03c917d215ca7925132dd675819418b3cceb563
Sibongiseni <sidubazjhb024@student.wethinkcode.co.za>
2025-10-27 22:39:02 +0200

    initialized async methods

--- zazu.py
+++ zazu.py
@@ -21,3 +21,29 @@ def load_config():
     with open('config.json', 'r') as f:
         config = json.load(f)
     return config
+
+async def handle_barge_in(decoded,twilio_ws,streamsid):
+    pass 
+
+async def handle_text(decoded,twilio_ws,sts_ws,streamsid):
+    pass
+
+async def sts_sender(sts_ws,audio_queue):
+    pass
+
+async def sts_receiver(sts_ws,twilio_ws,streamsid_queue):
+    pass
+
+async def twilio_receiver(twilio_ws,audio_queue,streamsid_queue):
+    pass
+
+async def twilio_handler(twilio_ws):
+    pass
+
+async def main():
+    await websockets.serve(twilio_handler, "localhost", 5000)
+    print("Twilio WebSocket server started on ws://localhost:5000")
+    await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
